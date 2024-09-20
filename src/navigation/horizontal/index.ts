4e12b7508f7c57c77c71c9e08eab8293bf1a4971
mohsen <ahmadi.edu@gmail.com>
2024-09-20 20:36:15 +0330

    Initial commit

---
+++ src/navigation/horizontal/index.ts
@@ -0,0 +1,5 @@
+import type { HorizontalNavItems } from "@layouts/types";
+import userManagement from "./userManagement";
+
+// export default [...dashboard, ...apps, ...pages, ...uiElements, ...forms, ...tables, ...charts, ...misc] as HorizontalNavItems
+export default [...userManagement] as HorizontalNavItems;
---
+++ src/plugins/webfontloader.ts
@@ -0,0 +1,24 @@
+/**
+ * plugins/webfontloader.js
+ *
+ * webfontloader documentation: https://github.com/typekit/webfontloader
+ */
+//NOTE - بدلیل استفاده مستقیم از فونت مورد نظر در فایل main
+//NOTE - این کامپوننت برای سبک تر شدن پروژه غیر فعال شده است
+// export async function loadFonts() {
+//   const webFontLoader = await import(/* webpackChunkName: "webfontloader" */'webfontloader')
+
+//   webFontLoader.load({
+//     // google: {
+//     //   families: ['Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'],
+//     // },
+//     custom: {
+//       families: ['Vazir'],
+//       urls: ['/src/assets/fonts/Vazir/vazirfont.css']
+//     }
+//   })
+// }
+
+// export default function () {
+//   loadFonts()
+// }
